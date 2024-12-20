f84d2a51315c5dbfacfe6ebc19f96a68a07ca6b8
nabaraj basnet <nabarajbasnet2000@gmail.com>
2024-12-20 15:58:16 +0545

    change random color of rectangle

--- boxcollision/script.js
+++ boxcollision/script.js
@@ -1,6 +1,11 @@
 
 const rectOne = document.getElementById('rect1');
 const ctx = rectOne.getContext('2d');
+const arrOfColors = ['black', 'white', 'green', 'yellow', 'pink', 'blue'];
+const randomIndexOfCarr = Math.random(arrOfColors.length) * arrOfColors.length;
+const fixedRandomIndexOfCArr = Math.ceil(randomIndexOfCarr);
+ctx.fillStyle = arrOfColors[fixedRandomIndexOfCArr];
+
 ctx.beginPath();
 const x = Math.random(5) * 470;
 const y = Math.random(5) * 470;
