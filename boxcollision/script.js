86724af4dd8dec502e9118bc77e4c058c6d07c00
nabaraj basnet <nabarajbasnet2000@gmail.com>
2024-12-20 16:32:17 +0545

    Generate two boxes in random position with random colors

--- boxcollision/script.js
+++ boxcollision/script.js
@@ -1,16 +1,38 @@
 
-const rectOne = document.getElementById('rect1');
-const ctx = rectOne.getContext('2d');
-const arrOfColors = ['black', 'white', 'green', 'yellow', 'pink', 'blue'];
-const randomIndexOfCarr = Math.random(arrOfColors.length) * arrOfColors.length;
-const fixedRandomIndexOfCArr = Math.ceil(randomIndexOfCarr);
-ctx.fillStyle = arrOfColors[fixedRandomIndexOfCArr];
+const boxOne = () => {
 
-ctx.beginPath();
-const x = Math.random(5) * 470;
-const y = Math.random(5) * 470;
-const fixedXvalue = Math.ceil(x);
-const fixedYvalue = Math.ceil(y);
+    const rectOne = document.getElementById('rect');
+    const ctx = rectOne.getContext('2d');
+    const arrOfColors = ["blue", "green", "yellow", "orange", "purple", "pink", "brown", "gray", "black", "white"];
+    const randomIndexOfCarr = Math.random(arrOfColors.length) * arrOfColors.length;
+    const fixedRandomIndexOfCArr = Math.ceil(randomIndexOfCarr);
+    ctx.fillStyle = arrOfColors[fixedRandomIndexOfCArr];
 
-ctx.rect(fixedXvalue, fixedYvalue, 30, 30);
-ctx.fill();
+    ctx.beginPath();
+    const x = Math.random(5) * 470;
+    const y = Math.random(5) * 470;
+    const fixedXvalue = Math.ceil(x);
+    const fixedYvalue = Math.ceil(y);
+
+    ctx.rect(fixedXvalue, fixedYvalue, 30, 30);
+    ctx.fill();
+}
+
+boxOne();
+
+const boxTwo = () => {
+    const rectTwo = document.getElementById('rect');
+    const ctx2 = rectTwo.getContext('2d');
+    const arrOfColors = ["blue", "green", "yellow", "orange", "purple", "pink", "brown", "gray", "black", "white"];
+    const randomIndex = Math.random(arrOfColors.length) * arrOfColors.length
+    const fixedRandomIndex = Math.ceil(randomIndex);
+    ctx2.fillStyle = arrOfColors.reverse()[fixedRandomIndex];
+    ctx2.beginPath();
+    const x = Math.random(5) * 470;
+    const y = Math.random(5) * 470;
+    const fixedX = Math.ceil(x);
+    const fixedY = Math.ceil(y);
+    ctx2.rect(fixedX, fixedY, 30, 30);
+    ctx2.fill();
+}
+boxTwo()
