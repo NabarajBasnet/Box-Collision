ea460ebaf8b0817c61d62efabffc83b011f2a741
nabaraj basnet <nabarajbasnet2000@gmail.com>
2024-12-20 15:52:27 +0545

    Prevent rectangle going outside of canvas

--- boxcollision/script.js
+++ boxcollision/script.js
@@ -2,10 +2,10 @@
 const rectOne = document.getElementById('rect1');
 const ctx = rectOne.getContext('2d');
 ctx.beginPath();
-const x = Math.random(5) * 500;
-const y = Math.random(5) * 500;
-const fixedXvalue = Math.ceil(x)
-const fixedYvalue = Math.ceil(y)
+const x = Math.random(5) * 470;
+const y = Math.random(5) * 470;
+const fixedXvalue = Math.ceil(x);
+const fixedYvalue = Math.ceil(y);
 
 ctx.rect(fixedXvalue, fixedYvalue, 30, 30);
 ctx.fill();
