d989fd0d41bb6627ba641cec9c061391df39dd49
nabaraj basnet <nabarajbasnet2000@gmail.com>
2025-01-29 23:01:45 +0545

    Boxes Created

--- boxes.js
+++ boxes.js
@@ -1,12 +1,36 @@
 const directions = [-1, 1];
 
-class Boxes {
-    constructor() { };
+class Box {
+    constructor(x, y, w, h) {
+        this.position = {
+            x: x || 0,
+            y: y || 0
+        };
 
-    draw() { };
-    move() { };
-    update() { };
-    checkCollision() { };
+        this.size = {
+            width: w || 0,
+            height: h || 0
+        };
+
+        this.speed = Math.random() * (3 - 0.5) + 0.5;
+    };
+
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.rect(this.position.x, this.position.y, this.size.width, this.size.height);
+        ctx.fillStyle = 'black';
+        ctx.fill();
+    };
+
+    move(ctx) {
+        this.position.x = this.position.x + directions[Math.floor(Math.random() * 2)] * this.speed;
+        this.position.y = this.position.y + directions[Math.floor(Math.random() * 2)] * this.speed;
+    };
+
+    update(ctx) {
+        this.draw(ctx);
+        this.move(ctx);
+    };
 };
 
-export default Boxes;
+export default Box;
--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+import Box from "./boxes.js";
 
-canvas.width = 600;
-canvas.height = 600;
-canvas.style.border = '2px solid black';
-canvas.style.backgroundColor = 'black';
+const documentHeight = document.documentElement.clientHeight / 1.6;
+const documentWidth = document.documentElement.clientWidth / 1.2;
+
+canvas.height = documentHeight;
+canvas.width = documentWidth;
+
+canvas.style.border = '2px solid gray';
+
+const boxes = Array.from({ length: 10 }, () => {
+    return new Box(
+        Math.random() * (canvas.width - 20),
+        Math.random() * (canvas.height - 20),
+        20,
+        20
+    );
+});
+
+const gameLoop = () => {
+
+    canvas.height = documentHeight;
+    canvas.width = documentWidth;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    boxes.forEach((box) => {
+        box.update(ctx);
+    });
+    requestAnimationFrame(gameLoop);
+};
+
+requestAnimationFrame(gameLoop);
